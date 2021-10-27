a363fc09030f4733afbb7613a4bd40e0d6d65791
6dba <pasha.panukov@gmail.com>
2021-10-27 19:06:36 +0300

    Try to logging

---
+++ config.py
@@ -0,0 +1,2 @@
+AUTH_URL = 'https://login.nstu.ru/ssoservice/XUI/#login/&realm=ido&goto=https%3A%2F%2Fdispace.edu.nstu.ru%2Fuser%2Fproceed%3Flogin%3Dopenam%26password%3Dauth'
+_XSRF = None
--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import argparse
-import os
+import os, sys
 import requests
+import config
 
+from fake_useragent import UserAgent
 from cryptocode import encrypt, decrypt
+from requests.sessions import session
 
-def parser_args() -> argparse.ArgumentParser:
+# decrypt(os.getenv('LOGIN'), os.getenv('KEY'))
+
+def _parse_args() -> argparse.ArgumentParser:
+    """
+    Обработка параметров
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument('-l','--login', 
                         type=str, 
@@ -15,22 +23,50 @@ def parser_args() -> argparse.ArgumentParser:
     return parser
 
 
-if __name__ == "__main__":
-    
-    parser = parser_args()
-    args = parser.parse_args()
-   
-    os.environ['KEY'] = '666'
-
+def init(args: argparse.Namespace) -> bool:
+    """
+    Инициализация переменных окружения, проверка на наличие аргументов
+    """    
     if args.login and args.password:
-        
+        os.environ['KEY'] = '666'
         os.environ['LOGIN'] = encrypt(args.login, os.getenv('KEY'))
         os.environ['PASSWORD'] = encrypt(args.password, os.getenv('KEY'))
 
+        return True
+    
+    return False
 
-        print(os.getenv('LOGIN'), decrypt(os.getenv('LOGIN'), os.getenv('KEY')))
-        
-    else:
-        print('Небходим запуск с параметрами -l (--login) [электронная почта/логин] -p (--password) [пароль] \
+
+def logging(login: str, password: str) -> None:
+    """
+    Авторизация на сайте
+    """ 
+    agent = UserAgent()
+    header = {'User-Agent': str(agent.chrome)}
+
+    with requests.Session() as session:
+        session.get(url=config.AUTH_URL, headers=header)
+        session.headers.update({'Referer': config.AUTH_URL})
+
+        config._XSRF = session.cookies.get('_xsrf', domain='.nstu.ru')
+
+        entrance = session.post(config.AUTH_URL, {
+            'backUrl': 'https://dispace.edu.nstu.ru/',
+            'username': login,
+            'password': password,
+            '_xsrf':config._XSRF,
+            'remember':'yes',    
+        })
+       
+
+
+if __name__ == "__main__":
+    
+    parser = _parse_args()
+    args = parser.parse_args()
+
+    if not init(args=args):
+        sys.exit('Небходим запуск с параметрами -l (--login) [электронная почта/логин] -p (--password) [пароль] \
               \nНапример: python main.py -l mail@corp.nstu.ru -p password ')
 
+    logging(str(decrypt(os.getenv('LOGIN'), os.getenv('KEY'))), str(decrypt(os.getenv('PASSWORD'), os.getenv('KEY'))))
