0752fecb856db13bbb55db53100eeeb05314d4d8
6dba <pasha.panukov@gmail.com>
2021-11-02 19:59:17 +0300

    events received, try to connect

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 import json
 import getpass
 
+from json.decoder import JSONDecodeError
 from random import random
 from typing import Any
 from cryptocode import encrypt, decrypt
@@ -11,17 +12,17 @@ from requests.sessions import RequestsCookieJar, session
 
 
 
-def _parse_args() -> argparse.ArgumentParser:
+def init_parser() -> argparse.ArgumentParser:
     """
     Обработка параметров
     """
     parser = argparse.ArgumentParser()
     parser.add_argument('-l','--login', 
                         type=str, 
-                        help='Логин или электронная почта. Зашифровано')
+                        help='Логин или электронная почта')
     parser.add_argument('-p','--password',
                         type=str,
-                        help='Пароль от личного кабинета. Зашифровано')
+                        help='Пароль от личного кабинета')
     return parser
 
 
@@ -34,7 +35,7 @@ def load_config() -> Any:
             "AUTH_TOKEN_URL" : "https://login.nstu.ru/ssoservice/json/authenticate?realm=/ido&goto=https://dispace.edu.nstu.ru/user/proceed?login=openam&password=auth",
             "NSTU_TOKEN_URL" : "https://login.nstu.ru/ssoservice/json/authenticate",
             "DISPACE_TOKEN_URL" : "https://dispace.edu.nstu.ru/user/proceed?login=openam&password=auth",
-            "CALENDAR_URL" : "https://dispace.edu.nstu.ru/diclass/calendar/",
+            "CALENDAR_URL" : "https://dispace.edu.nstu.ru/diclass/calendar/proceed?action=get_event_by_role&group_id=false",
             "LOGIN" : None,
             "PASSWORD" : None
         }
@@ -89,7 +90,7 @@ def init(args: argparse.Namespace) -> bool:
     return False
 
 
-def authenticate(login: str, password: str) -> Any:
+def authentification(login: str, password: str) -> Any:
     """
     Авторизация на сайте
     """ 
@@ -137,7 +138,8 @@ def authenticate(login: str, password: str) -> Any:
         auth = session.post(url=config['NSTU_TOKEN_URL'], json=json)
 
         if not auth.ok:
-            sys.exit('Неудачная аутентификация!')
+            sys.stdout.write('Неудачная аутентификация!\n')
+            sys.exit(f'Код: {auth}\nОтвет от сервера: {auth.text}\n')
 
         session.cookies.set(name='NstuSsoToken', value=auth.json()['tokenId']) # Если данные верны и аутентификация успешна - устанавливаем токен платформы в куки
         response = session.post(url=config['DISPACE_TOKEN_URL']) # В заголовке ответа получаем Set-Cookie (dispace токен)
@@ -149,32 +151,76 @@ def authenticate(login: str, password: str) -> Any:
         return None
 
 
-def processing(session: requests.Session) -> None:
+def processing(session: requests.Session) -> Any:
     """
-    Обработка расписания
+    Обработка мероприятий расписания
     """
     # https://dispace.edu.nstu.ru/diclass/webinar/index/id_webinar
     # Возможно стоит забрать session_id, math_hash с CALENDAR_URL
     config = load_config()
-    
+   
+    if not session:
+        return None
+
     with session:
-        response = session.get(url=config['CALENDAR_URL'])
+        response = session.post(url=config['CALENDAR_URL'], data={
+            'action' : 'get_event_by_role',
+            'group_id' : 'false'
+        }
+                                )
+        if response.ok:
+            try:
+                response.json()
+            except JSONDecodeError:
+                sys.stdout.write('Неудачное получение списка мероприятий\n')
+                sys.exit(f'Код: {response}\nОтвет от сервера: {response.text}\n')
+                
+
+            sys.stdout.write('Успешно получен список мероприятий!\n')
+            return response.json()['events'], session
+        
+        sys.stdout.write('Неудачный запрос на получение списка мероприятий\n')
+        sys.exit(f'Код: {response}\nОтвет от сервера: {response.text}\n')
+        
+    
+def parse_schedule(events: list) -> Any:
+    """
+    Разбор возвращенного списка, извлечение мероприятия, если оно is_available
+    """
+    for event in events:
+        event = dict(event)
+        
+        if event['is_available']:
+            return event
+    
+    return None
+
+
+def connecting(event: dict, session: requests.Session):
+    """
+    Подключение к вебинару
+    """
+    if event['is_available'] and session:
+        with session.get(url=event['join_link'], allow_redirects=True, stream=True) as response:
+            pass
+
 
 
 if __name__ == "__main__":
     
-    parser = _parse_args()
+    parser = init_parser()
     args = parser.parse_args()
     
     if not init(args=args):
         sys.exit('Для старта необходимо наличие параметра -l (--login) [электронная почта/логин], либо -l -p (--password) [пароль] \
-              \nНапример: python main.py -l mail@stud.nstu.ru | python main.py -l mail@corp.nstu.ru -p password \
+              \nНапример: python main.py -l mail@stud.nstu.ru | python main.py -l mail@stud.nstu.ru -p password \
               \nПоследующий запуск возможен без параметров')
 
-    session = authenticate(
+    session = authentification(
         str(decrypt(os.getenv('LOGIN'), os.getenv('KEY'))), 
         str(decrypt(os.getenv('PASSWORD'), os.getenv('KEY')))
     )
 
     if session:
-        processing(session=session)
+        events, session = processing(session=session)
+        connecting(event=parse_schedule(events=events), session=session)
